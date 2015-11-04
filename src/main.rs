5025ddad622382015cb03817f1f28a0b44538cf2
Norbert Melzer <timmelzer@gmail.com>
2015-11-04 12:35:05 +0100

    Parsing input in main()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+#![feature(convert)]
+
+extern crate pwb_ws_15 as pwb;
+
+use std::io;
+
+use pwb::parser;
+use pwb::data::{Values,Storage};
+
 fn main() {
-    let foo = "Hello";
-    println!("{}", foo[1..].to_string());
+    let mut line = String::new();
+
+    let garbage = io::stdin().read_line(&mut line).unwrap();
+    let garbage = io::stdin().read_line(&mut line).unwrap();
+    let garbage = io::stdin().read_line(&mut line).unwrap();
+    let garbage = io::stdin().read_line(&mut line).unwrap();
+
+    let (values, storage, recipes, fluid) =
+        parser::input(line.as_str()).unwrap();
+
+    println!("{:?}", values);
+    println!("{:?}", storage);
+    println!("{:?}", recipes);
+    println!("{:?}", fluid);
 }
