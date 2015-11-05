e08829d26eeb94d19b6b58c7a7b1cf1452654ef9
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-11-05 15:10:56 +0000

    .
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@14 7977857f-3c09-44fd-8702-009d356df24b

--- src/data/values.rs
+++ src/data/values.rs
@@ -1,3 +1,5 @@
+use std::slice::Iter;
+
 /// A list of the items values.
 ///
 /// The list is indexed by the ID of an item.
@@ -11,5 +13,21 @@ impl Values {
         Values {
             val: content
         }
+    }
+
+    pub fn get_value(&self, id: usize) -> isize {
+        self.val[id]
+    }
+
+    pub fn as_vec(&self) -> Vec<isize> {
+        self.val.clone()
+    }
+
+    pub fn as_ref_vec(&self) -> &Vec<isize> {
+        self.val.as_ref()
+    }
+
+    pub fn iter(&self) -> Iter<isize> {
+        self.val.iter()
     }
 }
