ad9b962d72c2cc481da8e92276f543b3b382e952
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-11-22 12:52:12 +0000

    Probably switching to cpp?
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@25 7977857f-3c09-44fd-8702-009d356df24b

---
+++ cpp/src/Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nmelzer on 21.11.15.
+//
+
+#include "Node.h"
---
+++ cpp/src/Node.h
@@ -0,0 +1,14 @@
+//
+// Created by nmelzer on 21.11.15.
+//
+
+#ifndef PWB_13_NODE_H
+#define PWB_13_NODE_H
+
+namespace pwb15 {
+  class Node {
+
+  };
+}
+
+#endif //PWB_13_NODE_H
