f28de86d1fa255536b7f101d66104d53f49db98d
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-11-22 12:52:15 +0000

    .
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@29 7977857f-3c09-44fd-8702-009d356df24b

---
+++ src/data/recipes.rs
@@ -0,0 +1,73 @@
+use data::Storage;
+
+use std::slice::Iter;
+
+/// What recipes are available during this game round?
+#[derive(PartialEq,Eq,Debug,Default)]
+pub struct Recipes {
+    val: Vec<Recipe>,
+}
+
+/// A recipe determines what ressources are consumed during a craft and which
+/// ones are produced, also it can tell you how much fluid will get consumed
+/// during this process.
+#[derive(PartialEq,Eq,Debug,Default)]
+pub struct Recipe {
+    ingredients: Vec<usize>,
+    output: Vec<usize>,
+    fluid: usize,
+}
+
+impl Recipes {
+    pub fn new(content: Vec<Recipe>) -> Self {
+        Recipes {
+            val: content
+        }
+    }
+
+    pub fn count(&self) -> usize {
+        self.val.len()
+    }
+
+    pub fn iter(&self) -> Iter<Recipe> {
+        self.val.iter()
+    }
+}
+
+impl Recipe {
+    pub fn new(input: Vec<usize>, output: Vec<usize>, fluid: usize) -> Self {
+        Recipe {
+            ingredients: input,
+            output: output,
+            fluid: fluid
+        }
+    }
+
+    pub fn produce(&self, store: &Storage) -> Option<Storage> {
+        let nstore = store.clone();
+
+        for i in &self.ingredients {
+            if !nstore.consume(*i) {
+                return None;
+            }
+        }
+        if !nstore.heat(self.fluid) {
+            return None;
+        }
+        for i in &self.output {
+            nstore.produce(*i);
+        }
+        Some(nstore)
+    }
+
+    pub fn producable(&self, store: &Storage) -> bool {
+        let nstore = store.clone();
+
+        for i in &self.ingredients {
+            if !nstore.consume(*i) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
