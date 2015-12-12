fdf414cec4aff3c90ca62dfd8c0ef6f88e513cec
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-12-12 16:56:01 +0000

    .
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@52 7977857f-3c09-44fd-8702-009d356df24b

--- erlang_try/src/cr_storage.erl
+++ erlang_try/src/cr_storage.erl
@@ -11,17 +11,21 @@
 -author("nmelzer").
 
 %% API
--export([parse/1, add_fluid_to_storage/2]).
+-export([parse/1, add_fluid_to_storage/2, consume_item/2]).
 
--export_type([storage/0, fluid/0, item_id/0]).
+-export_type([storage_content/0, storage/0, fluid/0, item_id/0]).
 
-%% @type fluid(). Describes amounts of fluids.
+%% @type fluid() = non_neg_integer(). Describes amounts of fluids.
 -type(fluid() :: non_neg_integer()).
-%% @type item_id(). Describes how an items ID looks like.
+%% @type item_id() = non_neg_integer(). Describes how an items ID looks like.
 -type(item_id() :: non_neg_integer()).
 
--record(storage, {storage = [] :: list()
-  ,               fluid   = 0  :: fluid()}).
+%% @type storage_content(). Describes how the content of a storage actually looks.
+%%   It is safe to match on this type.
+-opaque(storage_content() :: list(non_neg_integer())).
+
+-record(storage, {storage = [] :: storage_content()
+  , fluid = 0 :: fluid()}).
 
 %% @type storage(). The datatype of an actual storrage.
 -opaque(storage() :: #storage{}).
@@ -42,3 +46,16 @@ parse(Line) ->
 -spec add_fluid_to_storage(Store :: storage(), Fluid :: fluid()) -> storage().
 add_fluid_to_storage(#storage{fluid = OldFluid} = S, AddFluid) ->
   S#storage{fluid = OldFluid + AddFluid}.
+
+consume_item(#storage{storage = C} = S, ItemID) ->
+  S#storage{storage = consume_item_from_list(C, ItemID)}.
+
+consume_item_from_list([0|_], 0) -> impossible;
+consume_item_from_list([H|T], 0) -> [H - 1|T];
+consume_item_from_list(List, ID) -> consume_item_from_list(List, ID, []).
+
+consume_item_from_list([], _, Acc)    -> lists:reverse(Acc);
+consume_item_from_list([0|_], 0, _)   -> impossible;
+consume_item_from_list([H|T], 0, Acc) -> lists:reverse(Acc, [H - 1|T]);
+consume_item_from_list([H|T], I, Acc) ->
+  consume_item_from_list(T, I - 1, [H|Acc]).
