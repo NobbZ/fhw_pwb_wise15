74b223d8d2095e84cda04b740ad68d4d889eff02
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-12-12 16:56:01 +0000

    Some tests for recipes
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@53 7977857f-3c09-44fd-8702-009d356df24b

--- erlang_try/src/cr_recipes.erl
+++ erlang_try/src/cr_recipes.erl
@@ -15,15 +15,19 @@
 
 -export_type([recipes/0, recipe/0, recipe_id/0]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% @type recipe(). A recipe holds the instruction to produce some output from
 %%   some input.
 -record(recipe, {consumes = [] :: list(non_neg_integer())
-  ,              produces = [] :: list(non_neg_integer())
-  ,              fluid_cost = 0 :: cr_storage:fluid()}).
+  , produces = [] :: list(non_neg_integer())
+  , fluid_cost = 0 :: cr_storage:fluid()}).
 -opaque recipe() :: #recipe{}.
 
 %% @type recipes(). Does contain all known recipes.
--record(recipes, {rcps = array:new() :: array()}). % Dialyzer 2.6 doesn't know about array/1
+-record(recipes, {rcps = [] :: list(recipe())}). % Dialyzer 2.6 doesn't know about array/1
 -opaque recipes() :: #recipes{}.
 
 -type recipe_id() :: non_neg_integer().
@@ -37,8 +41,7 @@ parse(Line) ->
   {ok, [Form]} = erl_parse:parse_exprs(Tokens),
   {value, ListOfValues, _} = erl_eval:expr(Form, []),
   ListOfRecipes = lists:map(fun convert_tuple_to_recipe/1, ListOfValues),
-  RelaxedArray = array:from_list(ListOfRecipes),
-  #recipes{rcps = array:fix(RelaxedArray)}.
+  #recipes{rcps = ListOfRecipes}.
 
 %% @doc Checks wether its argument is a valid {@link recipe()} or not.
 -spec is_recipe(term()) -> boolean().
@@ -58,15 +61,15 @@ is_recipes(_)                   -> false.
 -spec recipe_equals(recipe(), recipe()) -> boolean().
 recipe_equals(L, R) ->
   lists:sort(L#recipe.consumes) == lists:sort(R#recipe.consumes)
-  andalso lists:sort(L#recipe.produces) == lists:sort(R#recipe.produces)
-  andalso L#recipe.fluid_cost == R#recipe.fluid_cost.
+    andalso lists:sort(L#recipe.produces) == lists:sort(R#recipe.produces)
+    andalso L#recipe.fluid_cost == R#recipe.fluid_cost.
 
 %%% PRIVATE STUFF!
 
 %% @doc Converts a tuple to a proper {@link recipe()}.
 -spec convert_tuple_to_recipe(ListOfTuples :: {[cr_storage:item_id()],
-                                               [cr_storage:item_id()],
-                                               cr_storage:fluid()}) -> recipe().
+  [cr_storage:item_id()],
+  cr_storage:fluid()}) -> recipe().
 convert_tuple_to_recipe({ListIn, ListOut, Fluid}) ->
   #recipe{consumes = ListIn, produces = ListOut, fluid_cost = Fluid}.
 
@@ -81,3 +84,43 @@ correctify([], L) when is_list(L)     -> lists:reverse(L);
 correctify([$(|T], L) when is_list(L) -> correctify(T, [${|L]);
 correctify([$)|T], L) when is_list(L) -> correctify(T, [$}|L]);
 correctify([H|T], L) when is_list(L)  -> correctify(T, [H|L]).
+
+-ifdef(TEST).
+
+parse_empty_test() ->
+  Exp = #recipes{rcps = []},
+  Act = parse("[]"),
+  ?assertMatch(Exp, Act).
+
+parse_single_recipe_test() ->
+  Exp =
+    #recipes{rcps = [#recipe{produces = [1], consumes = [2], fluid_cost = 0}]},
+  Act = parse("[([2],[1],0)]"),
+  ?assertMatch(Exp, Act).
+
+parse_complex_test() ->
+  Exp = #recipes{rcps = [
+    #recipe{consumes = [1, 2], produces = [0], fluid_cost = 5},
+    #recipe{consumes = [], produces = [1, 2], fluid_cost = 100},
+    #recipe{consumes = [0], produces = [1, 2], fluid_cost = 1}]},
+  Act = parse("[([1,2],[0],5),([],[1,2],100),([0],[1,2],1)]"),
+  ?assertMatch(Exp, Act).
+
+is_recipe_test() ->
+  ?assert(is_recipe(#recipe{consumes = [], produces = [], fluid_cost = 0})),
+  lists:map(fun(R) -> ?assert(is_recipe(R)) end, [
+    #recipe{consumes = [1, 2], produces = [0], fluid_cost = 5},
+    #recipe{consumes = [], produces = [1, 2], fluid_cost = 100},
+    #recipe{consumes = [0], produces = [1, 2], fluid_cost = 1}]).
+
+is_not_recipe_test() ->
+  lists:map(fun(NR) -> ?assertNot(is_recipe(NR)) end, [
+    #recipe{consumes = a},
+    #recipe{produces = a},
+    #recipe{fluid_cost = a},
+    "FooBar",
+    atom,
+    42]).
+
+
+-endif.
