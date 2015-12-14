b1f0f17e9dc96a644a8d41ed6496262dce0d1cff
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2015-12-14 15:09:31 +0000

    Further improvements on prop_runner
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@65 7977857f-3c09-44fd-8702-009d356df24b

--- erlang_try/properties/cr_storage_props.erl
+++ erlang_try/properties/cr_storage_props.erl
@@ -10,7 +10,7 @@
 -author("nmelzer").
 
 %% API
--export([runner/0]).
+-export([]).
 
 -include("storage.hrl").
 
@@ -18,15 +18,15 @@
 
 -define(SAMPLES, 1000).
 
-runner() ->
-  io:format("can_consume_from_filled"),
-  proper:quickcheck(can_consume_from_filled(), ?SAMPLES),
-  io:format("cant_consume_from_empty"),
-  proper:quickcheck(cant_consume_from_empty(), ?SAMPLES),
-  io:format("consuming_an_item_leaves_one_item_less_in_sum"),
-  proper:quickcheck(consuming_an_item_leaves_one_item_less_in_sum(), ?SAMPLES),
-  io:format("producing_an_item_leaves_one_item_more_in_sum"),
-  proper:quickcheck(producing_an_item_leaves_one_item_more_in_sum(), ?SAMPLES).
+%% runner() ->
+%%   io:format("can_consume_from_filled"),
+%%   proper:quickcheck(can_consume_from_filled(), ?SAMPLES),
+%%   io:format("cant_consume_from_empty"),
+%%   proper:quickcheck(cant_consume_from_empty(), ?SAMPLES),
+%%   io:format("consuming_an_item_leaves_one_item_less_in_sum"),
+%%   proper:quickcheck(consuming_an_item_leaves_one_item_less_in_sum(), ?SAMPLES),
+%%   io:format("producing_an_item_leaves_one_item_more_in_sum"),
+%%   proper:quickcheck(producing_an_item_leaves_one_item_more_in_sum(), ?SAMPLES).
 
 storage() ->
   ?LET({Xs, F}, {non_empty(list(non_neg_integer())), non_neg_integer()},
@@ -51,11 +51,11 @@ filled_storage_and_index() ->
   ?LET({S, I}, {filled_storage(), non_neg_integer()},
     {S, I rem length(S#storage.storage)}).
 
-can_consume_from_filled() ->
+prop_can_consume_from_filled() ->
   ?FORALL({S, Item}, filled_storage_and_index(),
     cr_storage:consume_item(S, Item) /= impossible).
 
-cant_consume_from_empty() ->
+prop_cant_consume_from_empty() ->
   ?FORALL({S, Item}, storage_and_index(),
     cr_storage:consume_item(S, Item) == impossible).
 
@@ -67,7 +67,7 @@ check_consuming_an_item_leaves_one_item_less_in_sum(S, Item) ->
     impossible -> lists:nth(NewItem + 1, S#storage.storage) == 0
   end.
 
-consuming_an_item_leaves_one_item_less_in_sum() ->
+prop_consuming_an_item_leaves_one_item_less_in_sum() ->
   ?FORALL({S, Item}, {storage(), non_neg_integer()},
     check_consuming_an_item_leaves_one_item_less_in_sum(S, Item)).
 
@@ -75,6 +75,6 @@ check_producing_an_item_leaves_one_item_more_in_sum(S, I) ->
   lists:sum(S#storage.storage) + 1 ==
     lists:sum((cr_storage:produce_item(S, I))#storage.storage).
 
-producing_an_item_leaves_one_item_more_in_sum() ->
+prop_producing_an_item_leaves_one_item_more_in_sum() ->
   ?FORALL({S, Item}, filled_storage_and_index(),
     check_producing_an_item_leaves_one_item_more_in_sum(S, Item)).
