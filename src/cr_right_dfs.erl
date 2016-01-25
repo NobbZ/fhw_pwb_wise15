3499e74ea8a5d4403162a5fc354f5fcb5ae309b3
inf100760 <inf100760@7977857f-3c09-44fd-8702-009d356df24b>
2016-01-25 11:39:58 +0000

    Rightfold DFS
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_13@89 7977857f-3c09-44fd-8702-009d356df24b

--- src/cr_right_dfs.erl
+++ src/cr_right_dfs.erl
@@ -10,4 +10,36 @@
 -author("nmelzer").
 
 %% API
--export([]).
+-export([start/3, run/4]).
+
+start(Storage, Recipes, Values) ->
+  spawn_link(?MODULE, run, [Storage, Recipes, Values, []]).
+
+run(Storage, Recipes, Values, Path) ->
+  put(root_storage, Storage), % \
+  put(recipes, Recipes),      %  > Save for later use
+  put(values, Values),        % /
+  InitialValue = cr_storage:calc_value(Storage, Values),
+  put(max_score, InitialValue),
+  crftr_global_data:print_solution(InitialValue, []),
+  cr_recipes:foldr(fun reducer/3, {Storage, Path}, Recipes).
+
+reducer(Idx, R, {Storage, Path}) ->
+  CurPath = [Idx|Path],
+  CurStorage = cr_recipes:apply_to_storage(R, Storage),
+  case CurStorage of
+    impossible -> {};
+    _ ->
+      CurValue = case CurStorage of
+        error -> get(max_score) - 100;
+        _ -> cr_storage:calc_value(CurStorage, get(values))
+      end,
+      case CurValue > get(max_score) of
+        true ->
+          put(max_score, CurValue),
+          crftr_global_data:print_solution(CurValue, CurPath);
+        false -> void
+      end,
+      cr_recipes:foldr(fun reducer/3, {CurStorage, CurPath}, get(recipes))
+  end,
+  {Storage, Path}.
